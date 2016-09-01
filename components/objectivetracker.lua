d1e8c27f1a6de75cb2fa922c1df11ed129e0672b
Push Chen <littlepush+github@gmail.com>
2016-09-01 21:59:35 +0800

    update objective tracker

--- components/objectivetracker.lua
+++ components/objectivetracker.lua
@@ -3,251 +3,142 @@ local
     PushUIStyle, PushUIAPI, 
     PushUIConfig, PushUIFrames = unpack(select(2, ...))
 
-if PushUIConfig.ObjectiveTrackerFrameHook.enable == false then
-    return
+local PushUIFramesObjectiveTrackerHook = {}
+local OTH = PushUIFramesObjectiveTrackerHook
+OTH.name = "PushUIFramesObjectiveTrackerHook"
+local _quests = {}
+OTH._quests = _quests
+
+OTH._gainQuest = function()
+    local _oqc = #_quests
+    for i = 1, _oqc do _quests[i] = nil end
+    -- 1: questID, 2: title, 3: questLogIndex, 4: numObjectives, 
+    -- 5: requiredMoney, 6: isComplete, 7: startEvent, 8: isAutoComplete, 
+    -- 9: failureTime, 10: timeElapsed, 11: questType, 12: isTask, 
+    -- 13: isBounty, 14: isStory, 15: isOnMap, 16: hasLocalPOI
+    local _qc = GetNumQuestWatches()
+    for i = 1, _qc do
+        local _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16 = GetQuestWatchInfo(i)
+        _quests[i] = {
+            questID = _1,
+            title = _2,
+            questLogIndex = _3,
+            numObjectives = _4,
+            requiredMoney = _5,
+            isComplete = _6,
+            startEvent = _7,
+            isAutoComplete = _8,
+            failureTime = _9,
+            timeElapsed = _10,
+            questType = _11,
+            isTask = _12,
+            isBounty = _13,
+            isStory = _14,
+            isOnMap = _15,
+            hasLocalPOI = _16
+        }
+    end
 end
-local PushUIFrameObjectiveTrackerHook = PushUIFrames.Frame.Create("PushUIFrameObjectiveTrackerHook", PushUIConfig.ObjectiveTrackerFrameHook)
-PushUIFrameObjectiveTrackerHook.HookParent = PushUIConfig.ObjectiveTrackerFrameHook.parent.HookFrame
 
-local fOTHook = PushUIFrameObjectiveTrackerHook
+OTH._openQuestOnMap = function(block)
+    QuestMapFrame_OpenToQuestDetails(block.quest.questID)
+end
 
--- Create Scrll Quest Frame
-fOTHook.OTContainer = CreateFrame("Frame", "PushUIOTContainer", fOTHook)
-fOTHook.OTScrollView = CreateFrame("ScrollFrame", nil, fOTHook.OTContainer)
-fOTHook.OTScrollContent = CreateFrame("Frame", nil, fOTHook.OTScrollView)
+OTH._initBlock = function(block)
+    block:SetWidth(200)
+    block:SetHeight(24)
+    PushUIConfig.skinType(block)
 
-fOTHook.OTContainer.scrollframe = fOTHook.OTScrollView
-fOTHook.OTScrollView.content = fOTHook.OTScrollContent
-fOTHook.OTScrollView:SetScrollChild(fOTHook.OTScrollContent)
-fOTHook.OTScrollView:EnableMouseWheel(true)
-fOTHook.OTScrollView:SetScript("OnMouseWheel", function(_, delta)
-    local _step = PushUIConfig.ObjectiveTrackerFrameHook.wheelStep or 10
-    local _side = PushUIConfig.ObjectiveTrackerFrameHook.scrollside or 1
-    if _side > 0 then _side = 1
-    else _side = -1 end
+    local _fs = block:CreateFontString()
 
-    local _pos = _:GetVerticalScroll()
-    local _newpos = _pos + delta * _step * _side
-    if _newpos < 0 then _newpos = 0 end
-    _:SetVerticalScroll(_newpos)
-end)
+    local _fn = "Fonts\\ARIALN.TTF"
+    local _fsize = 14
+    local _foutline = "OUTLINE"
 
-PushUIFrameObjectiveTrackerHook.ToggleCanShow = true
-PushUIFrameObjectiveTrackerHook.Toggle = function(status)
-    PushUIFrameObjectiveTrackerHook.ToggleCanShow = status
-    if status then
-        fOTHook.OTContainer:Show()
+    _fs:SetFont(_fn, _fsize, _foutline)
+
+    -- align
+    local _align = "LEFT"
+    _fs:SetJustifyH(_align)
+
+    local _w = 180
+    local _h = _fsize
+    _fs:SetWidth(180)
+    _fs:SetHeight(_fsize)
+    _fs:ClearAllPoints()
+    _fs:SetPoint("TOPLEFT", block, "TOPLEFT", 10, -5)
+
+    block.text = _fs
+
+    block:EnableMouse(true)
+    block:SetScript("OnMouseDown", function(self, ...)
+        OTH._openQuestOnMap(self)
+    end)
+end
+
+OTH._formatBlock = function(block)
+    local _quest = block.quest
+    local _finishCount = 0
+    for i = 1, _quest.numObjectives do
+        local _, _, finished = GetQuestLogLeaderBoard(i, _quest.questLogIndex);
+        if finished then
+            _finishCount = _finishCount + 1
+        end
+    end
+    block.text:SetText(_quest.title.."  (".._finishCount.."/".._quest.numObjectives..")")
+
+    if _quest.isOnMap then
+        block.text:SetTextColor(unpack(PushUIColor.orange))
     else
-        fOTHook.OTContainer:Hide()
+        block.text:SetTextColor(unpack(PushUIColor.white))
     end
 end
 
-PushUIFrameObjectiveTrackerHook.ReSize = function(...)
-    -- ObjectiveTrackerFrame:SetWidth(160)
+OTH._blocks = {}
+OTH._showQuest = function()
+    local _qc = #_quests
+    local _bc = #OTH._blocks
 
-    local _w, _h, _x, _y = fOTHook.HookParent.GetBlockRect(1, 2)
-    local _p = PushUISize.padding
-
-    _x = _x * PushUISize.Resolution.scale
-    _y = _y * PushUISize.Resolution.scale
-    _w = _w * PushUISize.Resolution.scale
-    _h = _h * PushUISize.Resolution.scale
-    _p = _p * PushUISize.Resolution.scale
-
-    _w = _w + (_w - _h)
-
-    fOTHook.OTContainer:SetWidth(_w)
-    fOTHook.OTContainer:SetHeight(_h)
-    fOTHook.OTContainer:SetPoint("TOPLEFT", fOTHook.HookParent, "TOPLEFT", _x, _y)
-
-    -- Resize the inner scroll view
-    fOTHook.OTScrollView:SetAllPoints(fOTHook.OTContainer)
-
-    -- Resize the inner content view
-    local otbf = ObjectiveTrackerBlocksFrame
-    local _oth = otbf:GetHeight()
-
-    fOTHook.OTScrollContent:SetWidth(_w)
-    fOTHook.OTScrollContent:SetHeight(_oth)
-
-    otbf:SetWidth(_w - 2 * _p)
-    otbf:SetAllPoints(fOTHook.OTScrollContent)
-    otbf:SetPoint("TOPLEFT", 30, 0)
-end
-
-PushUIFrameObjectiveTrackerHook.SetHeaderText = function(text)
-    PushUIStyle.SetFontSize(text, 13)
-    PushUIStyle.SetFontOutline(text)
-    PushUIStyle.SetFontColor(text, unpack(PushUIColor.white))
-end
-PushUIFrameObjectiveTrackerHook.SetContentText = function(text)
-    PushUIStyle.SetFontSize(text, 12)
-    -- PushUIStyle.SetFontOutline(text)
-    PushUIStyle.SetFontColor(text, unpack(PushUIColor.white))
-end
-
-local function fixBlockHeight(block)
-    if block.shouldFix then
-        local height = block:GetHeight()
-
-        if block.lines then
-            for _, line in pairs(block.lines) do
-                if line:IsShown() then
-                    height = height + 4
-                end
-            end
+    for i = 1, _qc do
+        local _blockFrame = nil
+        if i <= _bc then
+            _blockFrame = OTH._blocks[i]
+        else
+            _blockFrame = CreateFrame("Frame", OTH.name.."Block"..i, UIParent)
+            OTH._initBlock(_blockFrame)
+            OTH._blocks[i] = _blockFrame
+            _bc = _bc + 1
         end
+        _blockFrame.quest = _quests[i]
+        _blockFrame:ClearAllPoints()
+        OTH._formatBlock(_blockFrame)
+        _blockFrame:Show()
+        _blockFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -10, -(i - 1) * 28 - 30)
+    end
 
-        block.shouldFix = false
-        block:SetHeight(height + 5)
-        block.shouldFix = true
+    if _qc < _bc then
+        for i = _qc + 1, _bc do
+            OTH._blocks[i]:Hide()
+        end
     end
 end
 
-PushUIFrameObjectiveTrackerHook.Init = function(...)
-    local hm = ObjectiveTrackerFrame.HeaderMenu
-    hm:SetScript("OnShow", hm.Hide)
-    hm:Hide()
+-- Hide objective tracker
+ObjectiveTrackerFrame:SetScript("OnShow", ObjectiveTrackerFrame.Hide)
+ObjectiveTrackerFrame:Hide()
 
-    local otbf = ObjectiveTrackerBlocksFrame
-    otbf:SetParent(fOTHook.OTScrollContent)
-    fOTHook.OTScrollView:SetVerticalScroll(0)
-
-    local ot = ObjectiveTrackerFrame
-    local BlocksFrame = ot.BlocksFrame
-
-    for _, headerName in pairs({"QuestHeader", "AchievementHeader", "ScenarioHeader"}) do
-        local header = BlocksFrame[headerName]
-        header.Background:Hide()
-        PushUIFrameObjectiveTrackerHook.SetHeaderText(header.Text)
-    end
-
-    do
-        local header = BONUS_OBJECTIVE_TRACKER_MODULE.Header
-
-        header.Background:Hide()
-        PushUIFrameObjectiveTrackerHook.SetHeaderText(header.Text)
-    end
-
-    hooksecurefunc(DEFAULT_OBJECTIVE_TRACKER_MODULE, "SetBlockHeader", function(_, block)
-        if not block.headerStyled then
-            PushUIFrameObjectiveTrackerHook.SetHeaderText(block.HeaderText)
-            block.headerStyled = true
-        end
-    end)
-
-    hooksecurefunc(QUEST_TRACKER_MODULE, "SetBlockHeader", function(_, block)
-        if not block.headerStyled then
-            PushUIFrameObjectiveTrackerHook.SetHeaderText(block.HeaderText)
-            block.headerStyled = true
-        end
-
-        local itemButton = block.itemButton
-
-        if itemButton and not itemButton.styled then
-            itemButton:SetNormalTexture("")
-            itemButton:SetPushedTexture("")
-
-            itemButton.HotKey:ClearAllPoints()
-            itemButton.HotKey:SetPoint("CENTER", itemButton, 1, 0)
-            itemButton.HotKey:SetJustifyH("CENTER")
-            PushUIFrameObjectiveTrackerHook.SetContentText(itemButton.HotKey)
-
-            itemButton.Count:ClearAllPoints()
-            itemButton.Count:SetPoint("TOP", itemButton, 2, -1)
-            itemButton.Count:SetJustifyH("CENTER")
-            PushUIFrameObjectiveTrackerHook.SetContentText (itemButton.Count)
-
-            itemButton.icon:SetTexCoord(.08, .92, .08, .92)
-            PushUIConfig.skinType(itemButton)
-
-            itemButton.styled = true
-        end
-    end)
-
-    hooksecurefunc(DEFAULT_OBJECTIVE_TRACKER_MODULE, "AddObjective", function(self, block)
-        if block.module == QUEST_TRACKER_MODULE or block.module == ACHIEVEMENT_TRACKER_MODULE then
-            local line = block.currentLine
-
-            local p1, a, p2, x, y = line:GetPoint()
-            line:SetPoint(p1, a, p2, x, y - 4)
-        end
-    end)
-
-    hooksecurefunc("ObjectiveTracker_AddBlock", function(block)
-        if block.lines then
-            for _, line in pairs(block.lines) do
-                if not line.styled then
-                    PushUIFrameObjectiveTrackerHook.SetContentText(line.Text)
-                    line.Text:SetSpacing(2)
-
-                    if line.Dash then
-                        PushUIFrameObjectiveTrackerHook.SetContentText(line.Dash)
-                    end
-
-                    line:SetHeight(line.Text:GetHeight())
-
-                    line.styled = true
-                end
-            end
-        end
-
-        if not block.styled then
-            block.shouldFix = true
-            hooksecurefunc(block, "SetHeight", fixBlockHeight)
-            block.styled = true
-        end
-    end)
-
-    -- [[ Bonus objective progress bar ]]
-
-    hooksecurefunc(BONUS_OBJECTIVE_TRACKER_MODULE, "AddProgressBar", function(self, block, line)
-        local progressBar = line.ProgressBar
-        local bar = progressBar.Bar
-        local icon = bar.Icon
-
-        if not progressBar.styled then
-            local label = bar.Label
-
-            bar.BarBG:Hide()
-
-            icon:SetMask(nil)
-            icon:SetDrawLayer("BACKGROUND", 1)
-            icon:ClearAllPoints()
-            icon:SetPoint("RIGHT", 35, 2)
-            --bar.newIconBg = F.ReskinIcon(icon)
-
-            bar.BarFrame:Hide()
-
-            --bar:SetStatusBarTexture(C.media.backdrop)
-
-            label:ClearAllPoints()
-            label:SetPoint("CENTER")
-            PushUIFrameObjectiveTrackerHook.SetContentText(label)
-
-            --local bg = F.CreateBDFrame(bar)
-            --bg:SetPoint("TOPLEFT", -1, 1)
-            --bg:SetPoint("BOTTOMRIGHT", 0, -2)
-
-            progressBar.styled = true
-        end
-
-        --bar.IconBG:Hide()
-        --bar.newIconBg:SetShown(icon:IsShown())
-    end)
-
-    -- ScenarioStageBlock:SetHeight(0)
-    -- -- ScenarioStageBlock:SetScript("OnShow", ScenarioStageBlock.Hide)
-    -- ScenarioStageBlock:Hide()
-
-
-    ObjectiveTrackerFrame:Hide()
-    PushUIConfig.skinType(fOTHook.OTContainer)
-    fOTHook.ReSize()
+OTH._onUpdate = function()
+    OTH._gainQuest()
+    OTH._showQuest()
 end
 
-PushUIAPI.RegisterEvent(
-    "PLAYER_ENTERING_WORLD", 
-    PushUIFrameObjectiveTrackerHook,
-    PushUIFrameObjectiveTrackerHook.Init
-)
+OTH._onUpdate()
+
+PushUIAPI.RegisterEvent("QUEST_LOG_UPDATE", OTH, OTH._onUpdate)
+PushUIAPI.RegisterEvent("QUEST_WATCH_LIST_CHANGED", OTH, OTH._onUpdate)
+PushUIAPI.RegisterEvent("QUEST_ACCEPTED", OTH, OTH._onUpdate)
+PushUIAPI.RegisterEvent("QUEST_AUTOCOMPLETE", OTH, OTH._onUpdate)
+PushUIAPI.RegisterEvent("QUEST_POI_UPDATE", OTH, OTH._onUpdate)
+PushUIAPI.RegisterEvent("QUEST_TURNED_IN", OTH, OTH._onUpdate)
+PushUIAPI.RegisterEvent("ZONE_CHANGED_NEW_AREA", OTH, OTH._onUpdate)
+PushUIAPI.RegisterEvent("ZONE_CHANGED", OTH, OTH._onUpdate)
