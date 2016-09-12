f88a584c73574fe2a8f83b0bb49fabb307602fa3
Push Chen <littlepush+github@gmail.com>
2016-09-12 14:33:37 +0800

    add net stat and fps dock

--- components/systemdock.lua
+++ components/systemdock.lua
@@ -5,6 +5,7 @@ local
 
 local _sysdockPrefixName = "PushUIFramesSystemDock"
 local _colorUsage = {0.11, 0.11, 0.13}
+local _colorSysStat = {0.81, 0.71, 0.5}
 
 local lcontainer = "PushUIFramesLeftDockContainer"
 local ltint = "PushUIFrameLeftTintContainer"
@@ -83,4 +84,77 @@ end
 _ltintContainer.Push(_addonUsageDock.tintBar)
 
 
+--
+-- System Status
+local _sysstatDock = PushUIFrames.DockFrame.CreateNewDock(
+    _sysdockPrefixName.."SysStat", _colorSysStat, "BOTTOM", _lpanelContainer, _ltintContainer)
+_sysstatDock.__name = _sysdockPrefixName.."SysStat"
+_sysstatDock.panelAvailable = false;
+PushUIConfig.skinTooltipType(_sysstatDock.floatPanel)
+
+local _netStatTitleLabel = PushUIFrames.Label.Create(_sysstatDock.__name.."NetTitleState", _sysstatDock.floatPanel)
+local _netStatLabel = PushUIFrames.Label.Create(_sysstatDock.__name.."NetState", _sysstatDock.floatPanel)
+_netStatTitleLabel.SetMaxLines(4)
+_netStatTitleLabel.SetForceWidth(150)
+_netStatTitleLabel.SetFont(nil, 14)
+_netStatTitleLabel.SetJustifyH("LEFT")
+_netStatTitleLabel:SetPoint("TOPLEFT", _sysstatDock.floatPanel, "TOPLEFT")
+_netStatLabel.SetMaxLines(4)
+_netStatLabel.SetForceWidth(100)
+_netStatLabel.SetFont(nil, 14)
+_netStatLabel.SetJustifyH("RIGHT")
+_netStatLabel:SetPoint("TOPRIGHT", _sysstatDock.floatPanel, "TOPRIGHT")
+
+local _fpsTitleLabel = PushUIFrames.Label.Create(_sysstatDock.__name.."FPSTitle", _sysstatDock.floatPanel)
+local _fpsLabel = PushUIFrames.Label.Create(_sysstatDock.__name.."FPS", _sysstatDock.floatPanel)
+_fpsTitleLabel.SetForceWidth(150)
+_fpsTitleLabel.SetFont(nil, 14)
+_fpsTitleLabel.SetJustifyH("LEFT")
+_fpsTitleLabel:SetPoint("TOPLEFT", _netStatTitleLabel, "BOTTOMLEFT")
+_fpsLabel.SetMaxLines(4)
+_fpsLabel.SetForceWidth(100)
+_fpsLabel.SetFont(nil, 14)
+_fpsLabel.SetJustifyH("RIGHT")
+_fpsLabel:SetPoint("TOPRIGHT", _netStatLabel, "BOTTOMRIGHT")
+
+_sysstatDock.floatPanel:SetWidth(250)
+
+_netStatTitleLabel.SetTextString("Downloading:\nUploading:\nLatency Home:\nLatency World:")
+_fpsTitleLabel.SetTextString("FPS:")
+_sysstatDock.floatPanel:SetHeight(_netStatTitleLabel:GetHeight() + _fpsTitleLabel:GetHeight())
+
+_sysstatDock.__gatherSysInfo = function()
+    local _down, _up, _lhome, _lworld = GetNetStats()
+    if _down == nil then _down = 0 end
+    if _up == nil then _up = 0 end
+    if _down >= 1024 then 
+        _down = _down / 1024
+        _down = ("%.2f"):format(_down).."MB/s"
+    else
+        _down = ("%.2f"):format(_down).."KB/s"
+    end
+    if _up >= 1024 then 
+        _up = _up / 1024
+        _up = ("%.2f"):format(_up).."MB/s"
+    else
+        _up = ("%.2f"):format(_up).."KB/s"
+    end
+    if _lhome == nil then _lhome = 0 end
+    if _lworld == nil then _lworld = 0 end
+    local _sys = _down.."\n".._up.."\n".._lhome.."ms\n".._lworld.."ms"
+
+    _netStatLabel.SetTextString(_sys)
+
+    _fpsLabel.SetTextString(("%.2f"):format(GetFramerate()))
+end
+
+_sysstatDock.__gatherSysInfo()
+_sysstatDock.__refreshTimer = PushUIFrames.Timer.Create(1, _sysstatDock.__gatherSysInfo)
+_sysstatDock.floatPanel.WillAppear = function()
+    _sysstatDock.__refreshTimer:StartTimer()
+end
+_sysstatDock.floatPanel.WillDisappear = function()
+    _sysstatDock.__refreshTimer:StopTimer()
+end
+_ltintContainer.Push(_sysstatDock.tintBar)
 
